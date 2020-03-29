97db819a201d5d44f5d67cb176b06a3341912558
Junaid Farooq <junaid@evercam.io>
2020-03-29 13:06:50 +0500

    Get Notes from API. && Attach events.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { Button, Container, Row, Col } from "reactstrap";
 import logo from './logo.svg';
 import './App.css';
 
+import ListNotes from "./components/ListNotes"
+
+import { fetchNotes, fetchNote, updateNote } from "./api"
+
 class App extends Component{
   constructor(props) {
     super(props)
@@ -12,6 +16,32 @@ class App extends Component{
       current_note_id: 0,
       is_creating: true
     }
+
+    this.handleItemClick = this.handleItemClick.bind(this)
+    this.handleAddNote = this.handleAddNote.bind(this)
+    this.getData = this.getData.bind(this)
+  }
+
+  handleItemClick(id) {
+    this.setState((prevState) => {
+      return {is_creating: false, current_note_id: id}
+    })
+    console.log("here")
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  async getData() {
+    let data = await fetchNotes()
+    this.setState({ notes: data })
+  }
+
+  handleAddNote() {
+    this.setState((prevState) => {
+      return {is_creating: true}
+    })
   }
 
   render() {
@@ -23,13 +53,20 @@ class App extends Component{
               <h2>RealTime Notes</h2>
             </Col>
             <Col xs="2">
-              <Button color="primary">Create a new note</Button>
+              <Button color="primary" onClick={this.handleAddNote}>Create a new note</Button>
             </Col>
           </Row>
 
           <Row>
             <Col xs="4">
-              <h3>List our notes here</h3>
+              <ListNotes notes={this.state.notes} handleItemClick={(id) => this.handleItemClick(id)} />
+            </Col>
+            <Col xs="8">
+              {
+                this.state.is_creating ?
+                "Creating now..." :
+                `Editing note with id: ${this.state.current_note_id}`
+              }
             </Col>
           </Row>
         </Container>
---
+++ frontend/src/api.js
@@ -0,0 +1,29 @@
+const url = "http://127.0.0.1:8000/api/v1/notes/"
+
+export const fetchNotes = async () => {
+  return fetch(url, {})
+    .then(res => res.json())
+    .then(data => {
+      return data
+    })
+}
+// export const fetchNote = (id) => {
+
+// }
+
+export const addNote = (note) => {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(note)
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+
+  return note
+}
